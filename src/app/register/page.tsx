f665183580b29d34a30414bcee560645f7754825
Bautista Gallardo <bautistaggallardo@gmail.com>
2023-08-25 00:31:57 -0300

    audate singup

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+"use client";
+import axios from "axios";
 
-function registerPage(){
-    return (
-        <div><h1>Register Page</h1></div>
-    )
+function registerPage() {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // cancels its default actions
+
+    // extract form data
+    const formData = new FormData(e.currentTarget);
+
+    //console.log(`name: ${name}, email: ${email}, password: ${password}`)
+    try {
+        const res = await axios.post('/api/auth/signup', {
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password')
+        })
+        console.log(res)
+    } catch (error) {
+        console.log(error)
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="bg-zinc-800 px-4 py-2 block mb-2"
+          type="text"
+          placeholder="UserName"
+          name="name"
+        />
+        <input
+          className="bg-zinc-800 px-4 py-2 block mb-2"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <input
+          className="bg-zinc-800 px-4 py-2 block mb-2"
+          type="text"
+          placeholder="*********"
+          name="password"
+        />
+        <button className="bg-indigo-500 px-4 py-2">Register</button>
+      </form>
+    </div>
+  );
 }
 
-export default registerPage
+export default registerPage;
