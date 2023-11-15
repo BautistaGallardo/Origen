5c1c23d23f17932bd0db070036f7c3dfef0f32ed
RoMalan02 <rociomalanernst@gmail.com>
2023-11-15 00:19:14 -0300

    login works

--- src/app/(login)/logIn/Login.tsx
+++ src/app/(login)/logIn/Login.tsx
@@ -9,22 +9,45 @@ import {
     TextInput,
     Title,
 } from "@mantine/core";
+import { useState } from "react";
 import { hasLength, isEmail, useForm } from "@mantine/form";
+import axios from "axios";
+import { useRouter } from 'next/navigation'
 
 export function LoginForm() {
-    const form = useForm({
-        initialValues: {
-            email: "",
-            password: "",
-        },
-        validate: {
-            email: isEmail("Escriba un email válido"),
-            password: hasLength(
-                { min: 8 },
-                "La contraseña debe tener al menos 8 caracteres"
-            ),
-        },
+    const router = useRouter();
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
     });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); // cancels its default actions
+
+        // extract form data
+        const formData = new FormData(e.currentTarget);
+
+        //console.log(`name: ${name}, email: ${email}, password: ${password}`)
+        try {
+            const res = await axios.post('./../api/auth/login', {
+                name: formData.get('name'),
+                email: formData.get('email'),
+                password: formData.get('password')
+            })
+            if (res.status === 200) {
+                router.push('/dashboard')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    };
     return (
         <Container className="bg-white p-20 rounded-2xl shadow-2xl mx-auto">
             <Box w={400} mx="auto">
@@ -32,20 +55,20 @@ export function LoginForm() {
                 <Text size="sm" align="center" c="dimmed">
                     Ingresa con tus datos
                 </Text>
-                <form className="mt-4 flex flex-col gap-2">
+                <form className="mt-4 flex flex-col gap-2" onSubmit={handleSubmit}>
                     <TextInput
                         withAsterisk
                         required
+                        onChange={handleChange}
                         label="Email"
                         name="email"
-                        {...form.getInputProps("email")}
                     />
                     <PasswordInput
                         withAsterisk
                         required
+                        onChange={handleChange}
                         label="Password"
                         name="password"
-                        {...form.getInputProps("password")}
                     />
                     <Text size="xs" align="right" c="dimmed">
                         ¿No tienes una cuenta? <Link href="./signUp" className="text-Custm_secondary ">Registrate</Link>
--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,4 +1,4 @@
-import { getEnvVariable, getErrorResponse} from "@/libs/helpers";
+import { getEnvVariable, getErrorResponse } from "@/libs/helpers";
 import { prisma } from "@/libs/prisma"
 import { signJWT } from "@/libs/token"
 import { LoginUserInput, LoginUserSchema } from "@/libs/validations/user.schema";
@@ -6,63 +6,64 @@ import { compare } from "bcrypt";
 import { NextRequest, NextResponse } from "next/server";
 import { ZodError } from "zod"
 import { whatRole } from "@/app/api/controllersUsers/users";
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
-export async function POST(req:NextRequest) {
-    try{
-      const body = (await req.json()) as LoginUserInput;
-      const data = LoginUserSchema.parse(body);
+export async function POST(req: NextRequest) {
+  try {
+    const body = (await req.json()) as LoginUserInput;
+    const data = LoginUserSchema.parse(body);
 
     const user = await prisma.user.findUnique({
       where: { email: data.email },
     });
 
-    if (!user || (await compare(data.password, user.password))) {
+    if (!user || !(await compare(data.password, user.password))) {
       return getErrorResponse(401, "Invalid email or password");
     }
-      
-      const JWT_EXPIRES_IN = getEnvVariable("JWT_EXPIRES_IN")
-      const rolUser = await whatRole(user.id) 
-      
-      const token = await signJWT(
-        {sub: user.id, rol: rolUser},
-        {exp: `${JWT_EXPIRES_IN}m`}
-      )
 
-      const tokenMaxAge = parseInt(JWT_EXPIRES_IN) * 60;
-      
-      const cookieOptions = {
-        name: "token",
-        value: token,
-        path: "/",
-        secure: process.env.NODE_ENV !== "development",
-        maxAge: tokenMaxAge,
-      }
+    const JWT_EXPIRES_IN = getEnvVariable("JWT_EXPIRES_IN")
+    const rolUser = await whatRole(user.id) // Esto no deberia venir de un controller sino de un servicio
 
-      const response = new NextResponse(
-        JSON.stringify({
-          status: "seccess",
-          token
-        }),
-        {
-          status: 200,
-          headers: {"Content-Type": "application/json"}
-        }
-      )
+    const token = await signJWT(
+      { sub: user.id, rol: rolUser },
+      { exp: `${JWT_EXPIRES_IN}m` }
+    )
 
-      await Promise.all([
-        response.cookies.set(cookieOptions),
-        response.cookies.set({
-          name: "logged-in",
-          value: "true",
-          maxAge: tokenMaxAge
-        })
-      ])
-      
-      return response;
-    }catch(error: any){
-      if(error instanceof ZodError){
-        return getErrorResponse(400, "failed validation", error)
-      }
-      return getErrorResponse(500, error.message)
+    const tokenMaxAge = parseInt(JWT_EXPIRES_IN) * 60;
+
+    const tokenCookie: ResponseCookie = {
+      name: "token",
+      value: token,
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: tokenMaxAge,
+      httpOnly: true,
     }
+
+    const response = new NextResponse(
+      JSON.stringify({
+        status: "success",
+        token
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" }
+      }
+    )
+
+    response.cookies
+      .set(tokenCookie)
+      .set({
+        name: "logged-in",
+        value: "true",
+        maxAge: tokenMaxAge,
+        httpOnly: true
+      })
+
+    return response;
+  } catch (error: any) {
+    if (error instanceof ZodError) {
+      return getErrorResponse(400, "failed validation", error)
+    }
+    return getErrorResponse(500, error.message)
+  }
 }
